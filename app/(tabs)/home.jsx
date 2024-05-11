8f7fd4421e68c02b86e88e39ad92ee129a7bd92e
Susmita-Dey <susmitadey475@gmail.com>
2024-05-11 22:05:54 +0530

    making home page

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,70 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, FlatList, Image, RefreshControl } from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import SearchInput from "../../components/SearchInput";
+import Trending from "../../components/Trending";
+import EmptyState from "../../components/EmptyState";
 
 const Home = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    // re-call videos -> if any new video appears
+    setRefreshing(false);
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={[{ id: 1 }, { id: 2 }, { id: 3 }]}
+        // data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <Text className="text-3xl text-white">{item.id}</Text>
+        )}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4 space-y-6">
+            <View className="justify-between items-start flex-row mb-6">
+              <View>
+                <Text className="font-pmedium text-sm text-gray-100">
+                  Welcome Back
+                </Text>
+                <Text className="text-2xl font-psemibold text-white">
+                  Susmita
+                </Text>
+              </View>
+
+              <View className="mt-1.5">
+                <Image
+                  source={images.logoSmall}
+                  className="w-9 h-10"
+                  resizeMode="contain"
+                />
+              </View>
+            </View>
+
+            <SearchInput />
+
+            <View className="w-full flex-1 pb-8 pt-5">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                Trending Videos
+              </Text>
+
+              {/* Here ?? meaning if the array doesn't exists then make posts as an empty array */}
+              <Trending posts={[{ id: 1 }, { id: 2 }, { id: 3 }] ?? []} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="Be the first one to upload a video"
+          />
+        )}
+        refreshControl={<RefreshControl />}
+      />
     </SafeAreaView>
   );
 };
